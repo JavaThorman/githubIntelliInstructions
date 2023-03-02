49ede5ce455a6cf63918e995bd201de062cb7e3e
Jonas <jtskola0911@gmail.com>
2023-03-02 17:00:32 +0100

    xcx

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Github --> Share project on github --> Repo name
+        // Git --> manage remotes --> link + remote name.
+
     }
 }
